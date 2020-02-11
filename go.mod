7a4f6e1b5589fd33828a603c29d721f4bcb7c5de
Sergei Stadnik <sergeisqa@gmail.com>
2020-02-11 13:33:50 -0700

    upd

---
+++ go.mod
@@ -0,0 +1,8 @@
+module pdf/m/v1
+
+go 1.13
+
+require (
+	github.com/jung-kurt/gofpdf v1.16.2
+	golang.org/x/tools v0.0.0-20200210192313-1ace956b0e17 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func main() {
+	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+	w, h := pdf.GetPageSize()
+	fmt.Printf("width=%v, height=%v\n", w, h)
+	pdf.AddPage()
+
+	pdf.SetFont("arial", "B", 38)
+	_, lineHt := pdf.GetFontSize()
+	
+	pdf.SetTextColor(255, 0, 0)
+	pdf.Text(0, lineHt, "PPPP")
+
+
+	pdf.SetFont("times", "", 16)
+	pdf.MoveTo(0, lineHt*2)
+	pdf.MultiCell(0, lineHt*1.5, "some text")
+
+
+
+	err := pdf.OutputFileAndClose("sergei.pdf")
+	if err != nil {
+		panic(err)
+	}
+}
